00daf6483d5d1d157ac1c5d8b126cbbf80baec5a
SishiCheng <70729850+SishiCheng@users.noreply.github.com>
2022-06-19 23:03:22 -0400

    Update sm3demo.c

--- demos/sm3/sm3demo.c
+++ demos/sm3/sm3demo.c
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c) 2014 - 2021 The GmSSL Project.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -50,25 +50,39 @@
 #include <string.h>
 #include <stdlib.h>
 #include <gmssl/sm3.h>
+#include <gmssl/sm2.h>
 
 
 int main(int argc, char **argv)
 {
+	/* sm3_hash
 	SM3_CTX sm3_ctx;
 	uint8_t buf[4096];
 	ssize_t len;
 	uint8_t dgst[32];
 	int i;
 
-	sm3_init(&sm3_ctx);
+	sha256_init(&sm3_ctx);
 	while ((len = fread(buf, 1, sizeof(buf), stdin)) > 0) {
-		sm3_update(&sm3_ctx, buf, len);
+		sha256_update(&sm3_ctx, buf, len);
 	}
-	sm3_finish(&sm3_ctx, dgst);
+	sha256_finish(&sm3_ctx, dgst);
 
 	for (i = 0; i < sizeof(dgst); i++) {
 		printf("%02x", dgst[i]);
 	}
 	printf("\n");
+	
 	return 0;
+	*/
+
+	SM2_KEY tmp;
+	//const uint8_t skey[64] = "4f16ab84f1d146f036332f30cc00d76c6b598c01887d88d935e728d221f4506e";
+	sm2_key_set_private_key(&tmp, "4f16ab84f1d146f036332f30cc00d76c6b598c01887d88d935e728d221f4506e");
+	uint8_t pkey[64];
+ 	memcpy(pkey, tmp.public_key.x, 32);
+    memcpy(pkey + 32, tmp.public_key.y, 32);
+	for (int i = 0; i < 64; i++) {
+		printf("%02x", pkey[i]);
+	}
 }
